1235693f17a785a64c410462e17f0886bb5373e3
Harshpreet Singh <harshpreets179@gmail.com>
2022-09-16 19:44:15 +0530

    committing more testing files

---
+++ test.py
@@ -0,0 +1,22 @@
+
+x = [
+    {
+        "id":1,
+        "content":"HTML"
+    },
+    {
+        "id":2,
+        "content":"CSS"
+    },
+    {
+        "id":3,
+        "content":"JS"
+    },
+    {
+        "id":4,
+        "content":"Python"
+    } 
+]
+
+for ele in x:
+    print(ele.get("id"))
