f983deda33262525921d2547ef8639209c9d6d2e
Harshpreet Singh <harshpreets179@gmail.com>
2022-09-17 23:07:48 +0530

    added logic to filter and display table

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from urllib import response
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import JSONResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
 import uvicorn
+import pandas as pd
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+
+df = pd.read_csv('data/collegeData.csv')
+cities = list(df["city"].unique())
+courseNames = list(df['courseName'])
+courseNames = list(map(lambda z: str(z).replace(" ","_"), courseNames))
+df['courseNames2'] = courseNames
+courseNames = list(df['courseNames2'].unique())
 
 @app.get('/',response_class=HTMLResponse)
 async def home(request: Request):
@@ -18,31 +25,27 @@ async def home(request: Request, name:str):
 
 @app.get('/dropdown', response_class=HTMLResponse)
 async def render_dropdown(request:Request):
-    dropdown_list = [
-        {
-            "id":1,
-            "content":"HTML"
-        },
-        {
-            "id":2,
-            "content":"CSS"
-        },
-        {
-            "id":3,
-            "content":"JS"
-        },
-        {
-            "id":4,
-            "content":"Python"
-        },
-        {
-            "id":5,
-            "content":"Java"
-        }
-    ]
     
-    context = {"request":request, "categories":dropdown_list}
+    context = {"request":request, "cities":cities, "courseNames":courseNames}
     return templates.TemplateResponse("dropdown_demo.html", context)
+
+@app.post("/upload", response_class=HTMLResponse)
+async def handle_form(request:Request,
+                      cities_choice:str=Form(...),
+                      course_choice:str=Form(...)):
+    df1 = df[df['city'] == cities_choice]
+    df1 = df1[df1['courseNames2']==course_choice]
+    
+    context = {"request":request, "results":df1}
+    if len(list(df1['fullName'])):
+        return templates.TemplateResponse("display_table.html", context)
+    else:
+        return JSONResponse(
+            status_code=401,
+            content={
+                "message":"No Results found"
+            }
+        )
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, debug=True)
